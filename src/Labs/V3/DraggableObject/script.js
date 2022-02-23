d475b01806e931641f8acea40fe2dcdea8730f64
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-02-23 18:50:08 +0000

    Update DraggableObject

--- src/Labs/V3/DraggableObject/script.js
+++ src/Labs/V3/DraggableObject/script.js
@@ -20,24 +20,31 @@ class MainScene extends Phaser.Scene {
 	}
 
 	preload() {
-		this.load.image("Green", Assets.Sprites.Triangulos.Green);
+		const quadrados = Assets.Sprites.Quadrados.sheet;
+		this.load.spritesheet("Boxes", quadrados.png, { frameWidth: quadrados.frameWidth, frameHeight: quadrados.frameHeight, });
 	}
 
 	create() {
 		const { width, height, middleWidth, middleHeight } = world;
-		const sprite = this.add.sprite(middleWidth, middleHeight, "Green");
 
-		sprite.setInteractive({ useHandCursor: true, });
-		this.input.setDraggable(sprite);
+		const boxGroup = this.physics.add.group({ collideWorldBounds: true, });
 
-		this.input.on("drag", (pointer, gameObject, dragX, dragY) => {
-			gameObject.setPosition(dragX, dragY);
-		});
+		for (let i = 0; i < 50; i++) {
+			const box = this.add.sprite(randomNumber(0, width), randomNumber(0, height), "Boxes", randomNumber(0, 8))
+			boxGroup.add(box);
+
+			box.setInteractive({ useHandCursor: true, });
+			this.input.setDraggable(box);
+		}
+
+		this.physics.add.collider(boxGroup, boxGroup);
+
+		this.input.on("dragstart", (pointer, gameObject) => gameObject.body.enable = false);
+		this.input.on("drag", (pointer, gameObject, dragX, dragY) => gameObject.setPosition(dragX, dragY));
+		this.input.on("dragend", (pointer, gameObject) => gameObject.body.enable = true);
 	}
 
-	update(time, delta) {
-		FPSDiv.innerHTML = game.loop.actualFps.toFixed(1);
-	}
+	update(time, delta) { FPSDiv.innerHTML = game.loop.actualFps.toFixed(1); }
 }
 
 const config = {
@@ -45,12 +52,13 @@ const config = {
 	height: world.height,
 	type: Phaser.Auto,
 	backgroundColor: "#000000",
-	// scale: {
-	// 	mode: Phaser.Scale.FIT,
-	// },
+	scale: {
+		mode: Phaser.Scale.FIT,
+	},
 	physics: {
 		default: "arcade",
 		arcade: {
+			gravity: { x: 0, y: 300, },
 			debug: true,
 		}
 	},
